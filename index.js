41f3a8bcf1e04c32adefdbd8a87751a3ccde1089
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-10 10:35:23 +0900

    Feat : create loadArticles to show article on front page

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// 로그인 상태에 따른 화면 변화 //
 // window.onload를 사용해 해당 페이지가 모두 로드된 후에 checkLogin 함수가 실행되도록 세팅
 window.onload = async function checkLogin() {
     var payload = localStorage.getItem("payload")
@@ -15,4 +16,22 @@ window.onload = async function checkLogin() {
         loginoutButton.innerText = "로그인"
         loginoutButton.setAttribute("onclick", "location.href='/login.html'")
     }
+}
+
+
+// 작성한 게시글 불러오기 //
+window.onload = async function loadArticles() {
+    articles = await getArticles()
+    const article_list = document.getElementById("articles")
+
+    articles.forEach(article => {
+        const newArticle = document.createElement("div")
+        const articleImage = document.createElement("img")
+        articleImage.setAttribute("src", `${backend_base_url}${article.image}`)
+        newArticle.setAttribute("id", article._id)
+        newArticle.innerText = article.title
+        newArticle.setAttribute("onclick", "articleDetail(this.id)")
+        newArticle.appendChild(articleImage)
+        article_list.appendChild(newArticle)
+    });
 }
